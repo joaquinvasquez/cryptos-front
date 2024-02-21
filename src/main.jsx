0e4a31714bb9ef51a6724fd23a2282a0926c68b1
Joaquin Vasquez <joaquinvasquez20@gmail.com>
2024-02-21 14:42:41 -0300

    css styling to sass

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AppProvider } from './context/AppContext.jsx'
-import '/src/assets/css/style.css'
+import '/src/assets/css/style.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <AppProvider>
